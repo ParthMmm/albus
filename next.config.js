9892ca0f4887f07cbfcac0e6d3110247c60e30ad
parthmangrola <parthmangrola@gmail.com>
2021-08-09 19:36:39 -0700

    :tada: Init commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    LAST_FM_KEY: "9fad01c4307703006aa5ebe8aded58bc",
+  },
+};
---
+++ pages/landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+function landing() {
+  return (
+    <div>
+      <Box bg="tomato" w="100%" h="15vh" p={4} color="white">
+        This is the Box
+      </Box>
+    </div>
+  );
+}
+
+export default landing;
