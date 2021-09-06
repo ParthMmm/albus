8a733485c84f78610811ff2217d270578b94c386
parthmangrola <parthmangrola@gmail.com>
2021-09-06 14:11:11 -0700

    :lock: Updated api keys for vercel

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const tagTopAlbumsFetch = `https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&format=json&limit=10${process.env.LAST_FM_KEY}`;
-const albumInfoFetch = `https://ws.audioscrobbler.com/2.0/?method=album.getinfo&format=json${process.env.LAST_FM_KEY}`;
-const albumSearchFetch = `https://ws.audioscrobbler.com/2.0/?method=album.search&format=json${process.env.LAST_FM_KEY}`;
-const chartTopArtists = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&format=json&limit=10${process.env.LAST_FM_KEY}`;
-const chartTopTracks = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json&limit=10${process.env.LAST_FM_KEY}`;
+const tagTopAlbumsFetch = `https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&format=json&limit=10${process.env.NEXT_PUBLIC_LAST_FM_KEY}`;
+const albumInfoFetch = `https://ws.audioscrobbler.com/2.0/?method=album.getinfo&format=json${process.env.NEXT_PUBLIC_LAST_FM_KEY}`;
+const albumSearchFetch = `https://ws.audioscrobbler.com/2.0/?method=album.search&format=json${process.env.NEXT_PUBLIC_LAST_FM_KEY}`;
+const chartTopArtists = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&format=json&limit=10${process.env.NEXT_PUBLIC_LAST_FM_KEY}`;
+const chartTopTracks = `https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json&limit=10${process.env.NEXT_PUBLIC_LAST_FM_KEY}`;
 
 export {
   tagTopAlbumsFetch,
