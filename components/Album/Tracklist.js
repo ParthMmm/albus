fde3fcaf13425ddbc5fa8655595d42974c12b52c
parthmangrola <parthmangrola@gmail.com>
2021-09-22 15:17:43 -0700

    :sparkles: Fixed tracklist times

--- components/Album/Tracklist.js
+++ components/Album/Tracklist.js
@@ -55,7 +55,19 @@ function Tracklist({ tracks }) {
 
 function convertTime(time) {
   if (time) {
-    return time.toString().replace(/(.{2})$/, ":$1");
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = ~~time % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+    if (hrs > 0) {
+      ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + String(mins).padStart(1, "0") + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+    // return time.toString().replace(/(.{2})$/, ":$1");
   }
 }
 
