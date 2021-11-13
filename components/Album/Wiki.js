47e41ea8c97a75af8f51b69a78af27647491ff15
parthmangrola <parthmangrola@gmail.com>
2021-11-12 17:56:25 -0800

    :lipstick:  UI changed and fixes

--- components/Album/Wiki.js
+++ components/Album/Wiki.js
@@ -8,26 +8,34 @@ import {
   useColorMode,
   Spacer,
 } from "@chakra-ui/react";
-import React from "react";
-import { MdShare } from "react-icons/md";
+import React, { useRef } from "react";
+import { MdShare, MdExpandLess } from "react-icons/md";
 import ShareButton from "./ShareButton";
 function Wiki({ summary }) {
   const [show, setShow] = React.useState(false);
   const { colorMode } = useColorMode();
+  const box = useRef(null);
 
-  const handleToggle = () => setShow(!show);
+  const handleToggle = (flag) => {
+    setShow(!show);
+    if (flag) {
+      // box.current.scrollIntoView({ behavior: "smooth" });
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
   return (
     <>
       {summary ? (
         <Box color={colorMode === "dark" ? "white" : "black"}>
           <Box d="flex" flexDir="row-reverse" m="4" justifyContent="flex-start">
             {" "}
-            <Box pl="2">
+            <Box pl="2" ref={box}>
               <Button
                 fontFamily="Helvetica"
                 fontWeight="semibold"
                 size="md"
-                onClick={handleToggle}
+                onClick={() => handleToggle()}
                 rounded="xl"
                 bg="gray.400"
               >
@@ -45,9 +53,22 @@ function Wiki({ summary }) {
                 fontFamily="Helvetica"
                 fontSize="xl"
                 dangerouslySetInnerHTML={{ __html: summary }}
+                // noOfLines={10}
               ></Text>
             </Collapse>
           </Box>
+          {show ? (
+            <Button
+              as={MdExpandLess}
+              float="right"
+              bg="none"
+              pr={2}
+              pb={2}
+              onClick={() => handleToggle(true)}
+            />
+          ) : (
+            <> </>
+          )}
         </Box>
       ) : (
         <Center p={10}>
